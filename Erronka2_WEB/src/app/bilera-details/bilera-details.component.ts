9e7b48ec26a289e03c7f70c01247bc9c88aa27cd
Mikel Martin Castelo <mikel.martinca@elorrieta-errekamari.com>
2025-01-24 10:50:22 +0100

    Bilera details error

--- Erronka2_WEB/src/app/bilera-details/bilera-details.component.ts
+++ Erronka2_WEB/src/app/bilera-details/bilera-details.component.ts
@@ -1,24 +1,20 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { TranslateModule } from '@ngx-translate/core';
-import { IReuniones } from '../interfaces/IReuniones';
-import { AuthService } from '../auth/auth.service';
-import { HomeService } from '../home/home.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatSelectModule } from '@angular/material/select';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatOptionModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDialog } from '@angular/material/dialog';
-import { ComponentType } from '@angular/cdk/portal';
-import { ImagenPipePipe } from '../auth/pipes/Imagen.pipe';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { HomeService } from '../home/home.service';
+import { IReuniones } from '../interfaces/IReuniones';
 
 @Component({
   selector: 'app-bilera-details',
@@ -34,8 +30,8 @@ import { ImagenPipePipe } from '../auth/pipes/Imagen.pipe';
     MatIconModule,
     MatInputModule,
     MatOptionModule,
-    MatDialogModule,
-    ImagenPipePipe
+    MatDialogModule
+
   ],
   templateUrl: './bilera-details.component.html',
   styleUrls: ['./bilera-details.component.css']
@@ -50,41 +46,25 @@ export class BileraDetailsComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
   ) {
-    this.lortuBilera();
-    this.bilera;
   }
 
   ngOnInit(): void {
-    console.log('Bilera details component');
-    this.lortuBilera();
+
+    this.homeS.getBileraByID(this.route.snapshot.paramMap.get('id')!).subscribe(
+      (response) => {
+        console.log('Bilera lortu da:', response);
+        this._bilera = response;
+      },
+      (error) => {
+        console.error('Errorea bilera lortzean:', error);
+      }
+    );
   }
 
   mostrarSnackbar(mensaje: string) {
     this.snackBar.open(mensaje, 'cerrar', { duration: 2000 });
   }
 
-  lortuBilera() {
-    this.route.paramMap.subscribe(params => {
-      const id = params.get('id');
-      if (!id) {
-        this.mostrarSnackbar('ID ez da aurkitu');
-        return;
-      }
-
-      console.log('ID de la bilera:', id);
-      this.homeS.getBileraByID(id).subscribe({
-        next: (bilera) => {
-          setTimeout(() => {
-            this._bilera = bilera; // Asignamos la bilera después del retraso
-          }, 1000); // Retraso de 1 segundo
-        },
-        error: () => {
-          this.mostrarSnackbar('Ez da bilera aurkitu');
-        }
-      });
-    }
-    );
-  }
   get bilera(): IReuniones {
     return this._bilera!;
   }
